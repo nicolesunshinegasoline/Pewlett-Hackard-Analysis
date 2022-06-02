7dfeadaf55e5900fe6af96bdc13a0215048031ee
Nicole Ardizzi <96365523+nicolesunshinegasoline@users.noreply.github.com>
2022-06-02 18:58:11 -0400

    retirement query

---
+++ queries.sql
@@ -0,0 +1,53 @@
+-- Determine Retirement Eligibility
+-- Anyone born between 1952 and 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+
+
+-- look only at how many employees were born in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+-- CREATE NEW TABLE
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+
+-- Check data ffrom new table
+SELECT * FROM retirement_info;
+
+
+
+
+
+
+
+
+
+
+

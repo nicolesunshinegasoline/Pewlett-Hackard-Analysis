4cde677e17ca0b3f784cfb5ac33f1004a2da062d
Nicole Ardizzi <96365523+nicolesunshinegasoline@users.noreply.github.com>
2022-06-02 18:05:15 -0400

    final edit

--- schema.sql
+++ schema.sql
@@ -42,3 +42,27 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+
+
+CREATE TABLE dept_employee (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+
+
+
+CREATE TABLE titles (
+    emp_no INT NOT NULL, 
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
